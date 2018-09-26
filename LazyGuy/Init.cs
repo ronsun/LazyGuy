dd081399750d9fe1f6f944adb13995d083598c60
ronsun <ron.sun@hotmail.com.tw>
2018-09-26 22:15:36 +0800

    Init environment

---
+++ LazyGuy/Init.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyGuy
+{
+    public class Init
+    {
+        public T Echo<T>(T input)
+        {
+            return input;
+        }
+    }
+}
