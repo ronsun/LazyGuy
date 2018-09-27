5d1231a91a07d04d2d258e81895b30b0104cef37
ronsun <ron.sun@hotmail.com.tw>
2018-09-27 22:56:11 +0800

    improve unit test for DictionaryExtensions.cs

--- LazyGuy.Tests/Extensions/DictionaryExtensionsTests_Positive.cs
+++ LazyGuy.Tests/Extensions/DictionaryExtensionsTests_Positive.cs
@@ -10,48 +10,43 @@ namespace LazyGuy.Extensions.Tests
         #region AddOrUpdate
 
         [Test()]
-        public void AddOrUpdateTest_AddToEmpty_ExpectedResultAndCount()
+        [TestCaseSource(nameof(AddOrUpdate_SuccessCases))]
+        public void AddOrUpdateTest_AddToEmpty_ExpectedResultAndCount(
+            Dictionary<string, string> target,
+            string stubKey,
+            string stubValue,
+            Dictionary<string, string> expected)
         {
             //arrange
-            string stubKey = "k";
-            string stubValue = "v";
-
-            string expectedKey = stubKey;
-            string expectedValue = stubValue;
-
-            var target = new Dictionary<string, string>();
 
             //act
             target.AddOrUpdate(stubKey, stubValue);
 
             //assert
-            target.Should().HaveCount(1);
-            target.Should().Contain(expectedKey, expectedValue);
+            target.Count.Should().Be(expected.Count);
+            target.Should().Equal(expected);
         }
 
-        [Test()]
-        public void AddOrUpdateTest_AddWithoutDuplicate_ExpectedResultAndCount()
+        private static List<object[]> AddOrUpdate_SuccessCases()
         {
-            //arrange
             string stubKey1 = "k1";
-            string stubValue1 = "v1";
             string stubKey2 = "k2";
+
+            string stubValue1 = "v1";
             string stubValue2 = "v2";
 
-            string expectedKey1 = stubKey1;
-            string expectedValue1 = stubValue1;
-            string expectedKey2 = stubKey2;
-            string expectedValue2 = stubValue2;
+            var cases = new List<object[]>()
+            {
+                // add one to empty dictionary
+                new object[]{ new Dictionary<string, string>(), stubKey1, stubValue1, new Dictionary<string, string> { [stubKey1] = stubValue1 } },
 
-            var target = new Dictionary<string, string>() { [stubKey1] = stubValue1 };
+                // add one to dictionary with different key
+                new object[]{ new Dictionary<string, string>() { [stubKey1] = stubValue1 }, stubKey2, stubValue2, new Dictionary<string, string> { [stubKey1] = stubValue1, [stubKey2] = stubValue2 } },
 
-            //act
-            target.AddOrUpdate(stubKey2, stubValue2);
-
-            //assert
-            target.Should().HaveCount(2);
-            target.Should().Contain(expectedKey1, expectedValue1);
-            target.Should().Contain(expectedKey2, expectedValue2);
+                // add one to dictionary with same key
+                new object[]{ new Dictionary<string, string>() { [stubKey1] = stubValue1 }, stubKey1, stubValue2, new Dictionary<string, string> { [stubKey1] = stubValue2 } },
+            };
+            return cases;
         }
 
         #endregion
