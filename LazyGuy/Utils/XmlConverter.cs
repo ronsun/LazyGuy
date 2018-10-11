9a740de71be61970459766b04bedcaa0b1a0509e
ronsun <ron.sun@hotmail.com.tw>
2018-10-12 00:32:21 +0800

    add XmlConverter.cs without test

---
+++ LazyGuy/Utils/XmlConverter.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace LazyGuy.Utils
+{
+    public class XmlConverter
+    {
+        public virtual string Serialize(object obj, Encoding encoding = null)
+        {
+            if (encoding == null)
+            {
+                encoding = Encoding.UTF8;
+            }
+
+            var serializer = new XmlSerializer(obj.GetType());
+
+            using (var ms = new MemoryStream())
+            {
+                serializer.Serialize(ms, obj);
+                return encoding.GetString(ms.ToArray());
+            }
+        }
+
+        public virtual T DeSerialize<T>(string xml, Encoding encoding = null)
+        {
+            if (encoding == null)
+            {
+                encoding = Encoding.UTF8;
+            }
+
+            using (var ms = new MemoryStream(encoding.GetBytes(xml)))
+            {
+                var xs = new XmlSerializer(typeof(T));
+                var result = xs.Deserialize(ms);
+                if (result == null)
+                {
+                    return default(T);
+                }
+
+                return (T)result;
+            }
+        }
+    }
+}
