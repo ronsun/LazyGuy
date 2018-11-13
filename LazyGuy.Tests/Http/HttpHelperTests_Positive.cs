2a1e69aed0061d9ccd1641278a6568aac54c17e9
ronsun <ron.sun@hotmail.com.tw>
2018-11-13 22:25:56 +0800

    refactoring tests

--- LazyGuy.Tests/Http/HttpHelperTests_Positive.cs
+++ LazyGuy.Tests/Http/HttpHelperTests_Positive.cs
@@ -11,61 +11,83 @@ namespace LazyGuy.Http.Tests
     [TestFixture()]
     public class HttpHelperTests
     {
-        #region Get
+        #region General
 
         [Test()]
-        public void GetTest_DefaultOptions_ParametersOfRequestMatchExpected()
+        [TestCase(WebRequestMethods.Http.Get)]
+        [TestCase(WebRequestMethods.Http.Post)]
+        public void GeneralTest_DefaultOptions_RequestOptionsIsDefault(string httpVerb)
         {
             // arrange
-            var stubResponseString = "response";
-            var stubUrl = $"http://{nameof(GetTest_DefaultOptions_ParametersOfRequestMatchExpected)}";
-            var mockRequest = SetupFakeRequest(stubResponseString, stubUrl);
+            var stubPostConent = string.Empty;
+            var stubUrl = $"http://{nameof(GeneralTest_DefaultOptions_RequestOptionsIsDefault)}/{httpVerb}";
+            var mockRequest = SetupFakeRequest(stubUrl);
 
             var target = new HttpHelper();
 
-            string expectedMethod = "GET";
-            int expectedTimeout = 100000;
-            int expectedReadWriteTimeout = 300000;
-            string expectedContentType = "application/x-www-form-urlencoded";
+            var expectedOptions = new RequestOptions();
+            string expectedMethod = httpVerb;
             IWebProxy expectedProxy = mockRequest.Proxy;
 
             // act
-            target.Get(stubUrl);
+            if (httpVerb == WebRequestMethods.Http.Get)
+            {
+                target.Get(stubUrl);
+            }
+            else if (httpVerb == WebRequestMethods.Http.Post)
+            {
+                target.Post(stubUrl, stubPostConent);
+            }
             var actual = target.CurrentRequest;
 
             // assert
-            actual.Timeout.Should().Be(expectedTimeout);
-            actual.ReadWriteTimeout.Should().Be(expectedReadWriteTimeout);
-            actual.ContentType.Should().Be(expectedContentType);
+            actual.Timeout.Should().Be(expectedOptions.Timeout);
+            actual.ReadWriteTimeout.Should().Be(expectedOptions.ReadWriteTimeout);
+            actual.ContentType.Should().Be(expectedOptions.ContentType);
             actual.Method.Should().Be(expectedMethod);
             actual.Proxy.Should().BeEquivalentTo(expectedProxy);
         }
 
         [Test()]
-        public void GetTest_DefaultOptions_ResponseMatchExpected()
+        [TestCase(WebRequestMethods.Http.Get)]
+        [TestCase(WebRequestMethods.Http.Post)]
+        public void GeneralTest_DefaultOptions_ResponseMatchExpected(string httpVerb)
         {
             // arrange
-            var stubUrl = $"http://{nameof(GetTest_DefaultOptions_ResponseMatchExpected)}";
+            var stubPostConent = string.Empty;
+            var stubUrl = $"http://{nameof(GeneralTest_DefaultOptions_ResponseMatchExpected)}/{httpVerb}";
             var stubResponseString = "response";
 
-            SetupFakeRequest(stubResponseString, stubUrl);
+            SetupFakeRequest(stubUrl, stubResponseString);
 
             var target = new HttpHelper();
 
             string expected = stubResponseString;
+
             // act
-            var actual = target.Get(stubUrl);
+            string actual = null;
+            if (httpVerb == WebRequestMethods.Http.Get)
+            {
+                actual = target.Get(stubUrl);
+            }
+            else if (httpVerb == WebRequestMethods.Http.Post)
+            {
+                actual = target.Post(stubUrl, stubPostConent);
+            }
 
             // assert
+            actual.Should().NotBeNull();
             actual.Should().Be(expected);
         }
 
         [Test()]
-        public void GetTest_SetupOptionsWithoutProxy_ParametersOfRequestMatchExpected()
+        [TestCase(WebRequestMethods.Http.Get)]
+        [TestCase(WebRequestMethods.Http.Post)]
+        public void GeneralTest_SetupOptionsWithoutProxy_ParametersOfRequestMatchExpected(string httpVerb)
         {
             // arrange
-            var stubResponseString = "response";
-            var stubUrl = $"http://{nameof(GetTest_SetupOptionsWithoutProxy_ParametersOfRequestMatchExpected)}";
+            var stubPostConent = string.Empty;
+            var stubUrl = $"http://{nameof(GeneralTest_SetupOptionsWithoutProxy_ParametersOfRequestMatchExpected)}/{httpVerb}";
             var stubOptions = new RequestOptions()
             {
                 Encoding = Encoding.Default,
@@ -74,17 +96,24 @@ namespace LazyGuy.Http.Tests
                 ContentType = "contentType"
             };
 
-            var mockRequest = SetupFakeRequest(stubResponseString, stubUrl);
+            var mockRequest = SetupFakeRequest(stubUrl);
 
             var target = new HttpHelper();
 
-            string expectedMethod = "GET";
+            string expectedMethod = httpVerb;
             int expectedTimeout = stubOptions.Timeout;
             int expectedReadWriteTimeout = stubOptions.ReadWriteTimeout;
             string expectedContentType = stubOptions.ContentType;
 
             // act
-            target.Get(stubUrl, stubOptions);
+            if (httpVerb == WebRequestMethods.Http.Get)
+            {
+                target.Get(stubUrl, stubOptions);
+            }
+            else if (httpVerb == WebRequestMethods.Http.Post)
+            {
+                target.Post(stubUrl, stubPostConent, stubOptions);
+            }
             var actual = target.CurrentRequest;
 
             // assert
@@ -112,22 +141,33 @@ namespace LazyGuy.Http.Tests
         /// for detail (line 624 to line 626) if there are any concern.
         /// </summary>
         [Test()]
-        public void GetTest_SetupProxyOnly_ThrowExpectedException()
+        [TestCase(WebRequestMethods.Http.Get)]
+        [TestCase(WebRequestMethods.Http.Post)]
+        public void GeneralTest_SetupProxyOnly_ThrowExpectedException(string httpVerb)
         {
             // arrange
-            var stubResponseString = "response";
-            var stubUrl = $"http://{nameof(GetTest_SetupProxyOnly_ThrowExpectedException)}";
+            var stubPostConent = string.Empty;
+            var stubUrl = $"http://{nameof(GeneralTest_SetupProxyOnly_ThrowExpectedException)}/{httpVerb}";
             var stubOptions = new RequestOptions();
             stubOptions.SetDefaultProxy();
 
-            var mockRequest = SetupFakeRequest(stubResponseString, stubUrl);
+            var mockRequest = SetupFakeRequest(stubUrl);
 
             var target = new HttpHelper();
 
             // act
-            Action targetAction = () => target.Get(stubUrl, stubOptions);
+            Action targetAction = null;
+            if (httpVerb == WebRequestMethods.Http.Get)
+            {
+                targetAction = () => target.Get(stubUrl, stubOptions);
+            }
+            else if (httpVerb == WebRequestMethods.Http.Post)
+            {
+                targetAction = () => target.Post(stubUrl, stubPostConent, stubOptions);
+            }
 
             // assert
+            targetAction.Should().NotBeNull();
             targetAction.Should()
                         .Throw<ArgumentNullException>()
                         .WithMessage("Value cannot be null.*Parameter name: address");
@@ -141,7 +181,7 @@ namespace LazyGuy.Http.Tests
         /// <param name="stubResponseString"></param>
         /// <param name="stubUrl">Must be defference everytime.</param>
         /// <returns></returns>
-        private HttpWebRequest SetupFakeRequest(string stubResponseString, string stubUrl)
+        private HttpWebRequest SetupFakeRequest(string stubUrl, string stubResponseString = "response")
         {
             var stubResponseStream = new MemoryStream(Encoding.UTF8.GetBytes(stubResponseString));
             var stubResponse = Substitute.For<HttpWebResponse>();
@@ -150,6 +190,8 @@ namespace LazyGuy.Http.Tests
             var mockRequest = Substitute.ForPartsOf<HttpWebRequest>();
             mockRequest.When(r => r.GetResponse()).DoNotCallBase();
             mockRequest.GetResponse().Returns(stubResponse);
+            mockRequest.When(r => r.GetRequestStream()).DoNotCallBase();
+            mockRequest.GetRequestStream().Returns(new MemoryStream());
             mockRequest.Headers = new WebHeaderCollection();
 
             var stubRequestCreater = Substitute.For<IWebRequestCreate>();
