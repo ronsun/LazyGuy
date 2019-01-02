e062428aab087d59d05e517a14cd6b866ec09ddb
ronsun <ron.sun@hotmail.com.tw>
2019-01-03 00:01:50 +0800

    add X509Certificate2Extensions.cs

---
+++ src/LazyGuy/Extensions/X509Certificate2Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LazyGuy.Extensions
+{
+    /// <summary>
+    /// Extension methods for X509Certificate2
+    /// </summary>
+    public static class X509Certificate2Extensions
+    {
+        /// <summary>
+        /// Populates an X509Certificate2 object
+        /// with data from embedded resource in assembly.
+        /// </summary>
+        /// <param name="cert"></param>
+        /// <param name="assembly">The assembly contain the target embedded resource. </param>
+        /// <param name="fullName">Full name of target embedded resource. </param>
+        /// <returns></returns>
+        public static X509Certificate2 Import(this X509Certificate2 cert, Assembly assembly, string fullName)
+        {
+            using (Stream certStream = assembly.GetManifestResourceStream(fullName))
+            {
+                byte[] rawBytes = new byte[certStream.Length];
+                for (int index = 0; index < certStream.Length; index++)
+                {
+                    rawBytes[index] = (byte)certStream.ReadByte();
+                }
+
+                cert.Import(rawBytes);
+            }
+
+            return cert;
+        }
+    }
+}
