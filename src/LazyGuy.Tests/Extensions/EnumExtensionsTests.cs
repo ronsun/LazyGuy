d2fc6cf3685a2c55856eb6fc5e2c10b320ab2784
ronsun <ron.sun@hotmail.com.tw>
2019-04-08 23:38:52 +0800

    bug fix

--- src/LazyGuy.Tests/Extensions/EnumExtensionsTests.cs
+++ src/LazyGuy.Tests/Extensions/EnumExtensionsTests.cs
@@ -35,23 +35,23 @@ namespace LazyGuy.Extensions.Tests
         public void InTest_EnumInParames_ReturnTrue()
         {
             // arrange
-            var target = FakeEnum_In_A.A1;
+            var target = FakeEnum_In_A.A2;
 
             // act
-            bool actual = target.In(FakeEnum_In_A.A2, FakeEnum_In_A.A3);
+            bool actual = target.In(FakeEnum_In_A.A1, FakeEnum_In_A.A2, FakeEnum_In_A.A3);
 
             // assert
-            actual.Should().BeFalse();
+            actual.Should().BeTrue();
         }
 
         [Test()]
-        public void InTest_EmptyParames_ReturnTrue()
+        public void InTest_EmptyParames_ReturnFalse()
         {
             // arrange
-            var target = FakeEnum_In_A.A1;
+            var target = FakeEnum_In_A.A3;
 
             // act
-            bool actual = target.In(FakeEnum_In_A.A2);
+            bool actual = target.In();
 
             // assert
             actual.Should().BeFalse();
