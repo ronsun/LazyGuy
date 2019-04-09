d2bcb817a13eff6cfa40dcf1df4399e83d7c65db
ronsun <ron.sun@hotmail.com.tw>
2019-04-09 22:57:56 +0800

    implement extensions for enum by generic

--- src/LazyGuy/Extensions/EnumExtensions.cs
+++ src/LazyGuy/Extensions/EnumExtensions.cs
@@ -5,7 +5,8 @@ namespace LazyGuy.Extensions
 {
     public static class EnumExtensions
     {
-        public static bool In(this Enum target, params Enum[] list)
+        public static bool In<T>(this T target, params T[] list)
+            where T : Enum
         {
             Argument.ArrayTypeMatched(list, nameof(list), target.GetType());
             Argument.EnumDefined(target, nameof(target));
